1ba5966d8a0a4593c8eecf3197fbd734968033a4
Apurva Patel <apurvapatel6136@gmail.com>
2020-07-12 15:44:09 +0530

    Add files via upload

---
+++ Box_office_Analysis.py
@@ -0,0 +1,418 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h2 align=center>Data Visualization and Analysis of Worldwide Box Office Revenue </h2>
+# <img src="revenue.png">
+
+# #Libraries
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+pd.set_option('max_columns', None)
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+plt.style.use('ggplot')
+import datetime
+import lightgbm as lgb
+from scipy import stats
+from scipy.sparse import hstack, csr_matrix
+from sklearn.model_selection import train_test_split, KFold
+from wordcloud import WordCloud
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.util import ngrams
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.preprocessing import StandardScaler
+import nltk
+nltk.download('stopwords')
+stop = set(stopwords.words('english'))
+import os
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
+import xgboost as xgb
+import lightgbm as lgb
+from sklearn import model_selection
+from sklearn.metrics import accuracy_score
+import json
+import ast
+from urllib.request import urlopen
+from PIL import Image
+from sklearn.preprocessing import LabelEncoder
+import time
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
+
+
+# #Data Loading and Exploration
+
+# In[2]:
+
+
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+
+
+# In[3]:
+
+
+train.head()
+
+
+#  
+
+#  
+
+# #Visualizing the Target Distribution
+
+# In[4]:
+
+
+fig, ax = plt.subplots(figsize = (16, 6))
+plt.subplot(1, 2, 1)
+plt.hist(train['revenue']);
+plt.title('Distribution of revenue');
+plt.subplot(1, 2, 2)
+plt.hist(np.log1p(train['revenue']));
+plt.title('Distribution of log of revenue');
+
+
+# In[5]:
+
+
+train['log_revenue'] = np.log1p(train['revenue'])
+
+
+#   
+
+# #Relationship between Film Revenue and Budget
+
+# In[6]:
+
+
+fig, ax = plt.subplots(figsize = (16, 6))
+plt.subplot(1, 2, 1)
+plt.hist(train['budget']);
+plt.title('Distribution of budget');
+plt.subplot(1, 2, 2)
+plt.hist(np.log1p(train['budget']));
+plt.title('Distribution of log of budget');
+
+
+# In[7]:
+
+
+plt.figure(figsize=(16, 8))
+plt.subplot(1, 2, 1)
+plt.scatter(train['budget'], train['revenue'])
+plt.title('Revenue vs budget');
+plt.subplot(1, 2, 2)
+plt.scatter(np.log1p(train['budget']), train['log_revenue'])
+plt.title('Log Revenue vs log budget');
+
+
+# In[8]:
+
+
+train['log_budget'] = np.log1p(train['budget'])
+test['log_budget'] = np.log1p(test['budget'])
+
+
+#  
+
+# #Does having an Official Homepage Affect Revenue?
+
+# In[9]:
+
+
+train['homepage'].value_counts().head(10)
+
+
+# In[10]:
+
+
+train['has_homepage'] = 0
+train.loc[train['homepage'].isnull() == False, 'has_homepage'] = 1
+test['has_homepage'] = 0
+test.loc[test['homepage'].isnull() == False, 'has_homepage'] = 1
+
+
+# In[11]:
+
+
+sns.catplot(x='has_homepage', y='revenue', data=train);
+plt.title('Revenue for film with and without homepage');
+
+
+#  
+
+# #Distribution of Languages in Film
+
+# In[12]:
+
+
+plt.figure(figsize=(16, 8))
+plt.subplot(1, 2, 1)
+sns.boxplot(x='original_language', y='revenue', data=train.loc[train['original_language'].isin(train['original_language'].value_counts().head(10).index)]);
+plt.title('Mean revenue per language');
+plt.subplot(1, 2, 2)
+sns.boxplot(x='original_language', y='log_revenue', data=train.loc[train['original_language'].isin(train['original_language'].value_counts().head(10).index)]);
+plt.title('Mean log revenue per language');
+
+
+#  
+
+# #Frequent Words in Film Titles and Discriptions
+
+# In[13]:
+
+
+plt.figure(figsize = (12, 12))
+text = ' '.join(train['original_title'].values)
+wordcloud = WordCloud(max_font_size=None, background_color='white', width=1200, height=1000).generate(text)
+plt.imshow(wordcloud)
+plt.title('Top words in titles')
+plt.axis("off")
+plt.show()
+
+
+# In[14]:
+
+
+plt.figure(figsize = (12, 12))
+text = ' '.join(train['overview'].fillna('').values)
+wordcloud = WordCloud(max_font_size=None, background_color='white', width=1200, height=1000).generate(text)
+plt.imshow(wordcloud)
+plt.title('Top words in overview')
+plt.axis("off")
+plt.show()
+
+
+# #Do Film Descriptions Impact Revenue?
+
+# In[15]:
+
+
+import eli5
+
+vectorizer = TfidfVectorizer(
+            sublinear_tf=True,
+            analyzer='word',
+            token_pattern=r'\w{1,}',
+            ngram_range=(1, 2),
+            min_df=5)
+
+overview_text = vectorizer.fit_transform(train['overview'].fillna(''))
+linreg = LinearRegression()
+linreg.fit(overview_text, train['log_revenue'])
+eli5.show_weights(linreg, vec=vectorizer, top=20, feature_filter=lambda x: x != '<BIAS>')
+
+
+# In[16]:
+
+
+print('Target value:', train['log_revenue'][1000])
+eli5.show_prediction(linreg, doc=train['overview'].values[1000], vec=vectorizer)
+
+
+#  
+
+# 
+
+# In[17]:
+
+
+test.loc[test['release_date'].isnull() == False, 'release_date'].head()
+
+
+# In[ ]:
+
+
+
+
+
+#  
+
+# #Preprocessing Features
+
+# In[18]:
+
+
+def fix_date(x):
+    year = x.split('/')[2]
+    if int(year) <= 19:
+        return x[:-2] + '20' + year
+    else:
+        return x[:-2] + '19' + year
+
+
+# In[19]:
+
+
+test.loc[test['release_date'].isnull() == True].head()
+
+
+# In[20]:
+
+
+test.loc[test['release_date'].isnull() == True, 'release_date'] = '05/01/00'
+
+
+# In[21]:
+
+
+train['release_date'] = train['release_date'].apply(lambda x: fix_date(x))
+test['release_date'] = test['release_date'].apply(lambda x: fix_date(x))
+
+
+#  
+
+# #Creating Features Based on Release Date
+
+# In[22]:
+
+
+train['release_date'] = pd.to_datetime(train['release_date'])
+test['release_date'] = pd.to_datetime(test['release_date'])
+
+
+# In[23]:
+
+
+def process_date(df):
+    date_parts = ["year", "weekday", "month", 'weekofyear', 'day', 'quarter']
+    for part in date_parts:
+        part_col = 'release_date' + "_" + part
+        df[part_col] = getattr(df['release_date'].dt, part).astype(int)
+    
+    return df
+
+train = process_date(train)
+test = process_date(test)
+
+
+#  
+
+# #Using Plotly to Visualize the Number of Films Per Year
+
+# In[24]:
+
+
+# Count no.of films released per year and sort the years in ascending order
+# Do this for both Train and Test Sets
+d1 = train['release_date_year'].value_counts().sort_index()
+d2 = test['release_date_year'].value_counts().sort_index()
+
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+
+# x values are years, and y values are movie counts, name=legend
+data = [go.Scatter(x=d1.index, y=d1.values, name='train'),
+        go.Scatter(x=d2.index, y=d2.values, name='test')]
+
+layout = go.Layout(dict(title = "Number of films per year",
+                  xaxis = dict(title = 'Year'),
+                  yaxis = dict(title = 'Count'),
+                  ),legend=dict(
+                orientation="v"))
+py.iplot(dict(data=data, layout=layout))
+
+
+# In[ ]:
+
+
+
+
+
+#  
+
+# #Number of Films and Revenue Per Year
+
+# In[25]:
+
+
+d1 = train['release_date_year'].value_counts().sort_index()
+d2 = train.groupby(['release_date_year'])['revenue'].sum()
+
+data = [go.Scatter(x=d1.index, y=d1.values, name='film count'),
+        go.Scatter(x=d2.index, y=d2.values, name='total revenue', yaxis='y2')]
+
+layout = go.Layout(dict(title = "Number of films and total revenue per year",
+                        xaxis = dict(title = 'Year'),
+                        yaxis = dict(title = 'Count'),
+                        yaxis2=dict(title='Total revenue', overlaying='y', side='right')),
+                   legend=dict(orientation="v"))
+
+py.iplot(dict(data=data, layout=layout))
+
+
+# In[26]:
+
+
+d1 = train['release_date_year'].value_counts().sort_index()
+d2 = train.groupby(['release_date_year'])['revenue'].mean()
+
+data = [go.Scatter(x=d1.index, y=d1.values, name='film count'),
+        go.Scatter(x=d2.index, y=d2.values, name='mean revenue', yaxis='y2')]
+
+layout = go.Layout(dict(title = "Number of films and average revenue per year",
+                  xaxis = dict(title = 'Year'),
+                  yaxis = dict(title = 'Count'),
+                  yaxis2=dict(title='Average revenue', overlaying='y', side='right')
+                  ),legend=dict(
+                orientation="v"))
+py.iplot(dict(data=data, layout=layout))
+
+
+#  
+
+# #Do Release Days Impact Revenue?
+
+# In[27]:
+
+
+sns.catplot(x='release_date_weekday', y='revenue', data=train);
+plt.title('Revenue on different days of week of release');
+
+
+# In[ ]:
+
+
+
+
+
+#  
+
+# #Relationship between Runtime and Revenue
+
+# In[28]:
+
+
+plt.figure(figsize=(15, 6))
+plt.subplot(1, 2, 1)
+sns.distplot(train['runtime'].fillna(0) / 60, bins=40, kde=False);
+plt.title('Distribution of length of film in hours');
+plt.subplot(1, 2, 2)
+sns.scatterplot(train['runtime'].fillna(0)/60, train['revenue'])
+plt.title('runtime vs revenue');
+
+
+#  
+
+# #Highest Grossing Genres
+
+# In[29]:
+
+
+sns.catplot(x='num_genres', y='revenue', data=train);
+plt.title('Revenue for different number of genres in the film');
+
+
+#  
